585c79a15dd7e579302f903b851340412915e01e
Sage <sage@crotown.com>
2026-04-19 02:05:37 -0400

    successful PWM control of all four motors

---
+++ drive.py
@@ -0,0 +1,37 @@
+#! /home/sage/Code/.venv/bin/python
+
+from gpiozero import Motor
+from gpiozero import Device
+from gpiozero.pins.rpigpio import RPiGPIOFactory
+import time
+
+Device.pin_factory = RPiGPIOFactory()
+
+motor1 = Motor(forward = 7, backward = 1)
+motor2 = Motor(forward = 20, backward = 21)
+motor3 = Motor(forward = 27, backward = 17)
+motor4 = Motor(forward = 9, backward = 10)
+
+motor1.forward()
+time.sleep(1)
+motor1.forward(speed = 0.5)
+time.sleep(1)
+motor1.stop()
+
+motor2.forward()
+time.sleep(1)
+motor2.forward(speed = 0.5)
+time.sleep(1)
+motor2.stop()
+
+motor3.forward()
+time.sleep(1)
+motor3.forward(speed = 0.5)
+time.sleep(1)
+motor3.stop()
+
+motor4.forward()
+time.sleep(1)
+motor4.forward(speed = 0.5)
+time.sleep(1)
+motor4.stop()
