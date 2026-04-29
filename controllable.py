3dd9b4045940a5aab1d6535c482ee6a30f8bb24c
Sage <sage@crotown.com>
2026-04-29 01:08:13 -0400

    Added strafing

--- controllable.py
+++ controllable.py
@@ -5,40 +5,29 @@ import evdev
 from gpiozero import Device, Motor
 from gpiozero.pins.rpigpio import RPiGPIOFactory
 import time
+import math
 
 Device.pin_factory = RPiGPIOFactory()
 
-# named top to bottom, left to right: 
+# motors are named top to bottom, left to right: 
 # 1  2
 # 3  4
-motor1 = Motor(forward=7, backward=1)
-motor2 = Motor(forward=20, backward=21)
-motor3 = Motor(forward=27,backward=17)
-motor4 = Motor(forward=9,backward=10)
+motor1 = Motor(7, 1)
+motor2 = Motor(20, 21)
+motor3 = Motor(27, 17)
+motor4 = Motor(9, 10)
 
-# motor1.forward()
-# time.sleep(1)
-# motor1.stop()
-# motor2.forward()
-# time.sleep(1)
-# motor2.stop()
-# motor3.forward()
-# time.sleep(1)
-# motor3.stop()
-# motor4.forward()
-# time.sleep(1)
-# motor4.stop()
-
-devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-for device in devices:
-	print(device.path, device.name, device.phys)
+# devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+# for device in devices:
+# 	print(device.path, device.name, device.phys)
 
 dev = evdev.InputDevice('/dev/input/event4')
 
-hat_x = 0
-hat_y = 0
+rot_x = 0
+rot_y = 0
 x = 0
 y = 0
+w = 0
 vel = 1
 
 def left(vel):
@@ -77,57 +66,90 @@ print("listening for controller input ... ")
 
 for event in dev.read_loop():
 	if event.type == ecodes.EV_ABS:
+		print(event.code == ecodes.ABS_RX)
 
-		if event.code == ecodes.ABS_HAT0X or event.code == ecodes.ABS_HAT0Y:
-
-			if event.code == ecodes.ABS_HAT0X:
-				hat_x = event.value
-				print("x: ", hat_x)
-			elif event.code == ecodes.ABS_HAT0Y:
-				hat_y = event.value
-				print("y: ", hat_y)
-
-			if hat_y == -1:
-				forward(vel)
-			elif hat_y == 1:
-				backward(vel)
-			elif hat_x == -1:
-				left(vel)
-			elif hat_x == 1:
-				right(vel)
-			elif hat_x == 0 and hat_y == 0:
-				stop()
-
-		elif event.code == ecodes.ABS_X or event.code == ecodes.ABS_Y:
-
-			if event.code == ecodes.ABS_X:
-				x = (event.value/32767)
-				print("x: ", x)
-			elif event.code == ecodes.ABS_Y:
-				y = (event.value/32767)
-				print("y: ", y)
-
-
-# TODO:
-# write functions to combine robot targets into motor signals
-# left joystick should control movement (foward, backward, strafe left, strafe right)
-# right joystick should control pose
-# will need mecanum equations for this bit
-# will need to rewrite drive equations
+		if event.code == ecodes.ABS_RX or event.code == ecodes.ABS_RY:
 			
-
-			if y < 0:
-				# print("forward")
-				# forward(abs(y))
-				pass
-			elif y > 0:
-				# print("backward")
-				# backward(y)
-				pass
+			if event.code == ecodes.ABS_RX:
+				w = -event.value/32767
 			
-			if x < 0:
-				left(abs(x))
-			elif x > 0:
-				right(x)
-			elif x == 0 and y == 0:
-				stop()
+			print("rot: ", w)
+			target1 = -w
+			target2 = w
+			target3 = -w
+			target4 = w
+
+		else:
+			if event.code == ecodes.ABS_HAT0X or event.code == ecodes.ABS_HAT0Y:
+
+				if event.code == ecodes.ABS_HAT0X:
+					y = -event.value
+					# print("y: ", y)
+				elif event.code == ecodes.ABS_HAT0Y:
+					x = -event.value
+					# print("x: ", x)
+
+			elif event.code == ecodes.ABS_X or event.code == ecodes.ABS_Y:
+
+				# translate from x to y, the controller has a different sense of direction
+				if event.code == ecodes.ABS_X:
+					y = -(event.value/32767)
+				
+				elif event.code == ecodes.ABS_Y:
+					x = -(event.value/32767)
+					
+			# print("input: (", x, ", ", y, ")")
+
+			# add z when I configure rotational inputs from controller	
+			target1 = (x + y) / 1.2 
+			target2 = (x - y) / 1.2
+			target3 = (x - y) / 1.2 
+			target4 = (x + y) / 1.2 
+
+		if target1 > 1:
+			target1 = 1
+		if target2 > 1:
+			target2 = 1
+		if target3 > 1:
+			target3 = 1
+		if target4 > 1:
+			target4 = 1
+
+		if target1 < -1:
+			target1 = -1
+		if target2 < -1:
+			target2 = -1
+		if target3 < -1:
+			target3 = -1
+		if target4 < -1:
+			target4 = -1
+
+		print("targets: (", target1, ", ", target2, ", ", target3, ", ", target4, ")")
+
+		if (target1 > 0):
+			motor1.forward(target1)
+		elif (target1 < 0): 
+			motor1.backward(-target1)
+		else:
+			motor1.stop()
+
+		if (target2 > 0):
+			motor2.forward(target2)
+		elif (target2 < 0): 
+			motor2.backward(-target2)
+		else:
+			motor2.stop()
+
+		if (target3 > 0):
+			motor3.forward(target3)
+		elif (target3 < 0): 
+			motor3.backward(-target3)
+		else:
+			motor3.stop()
+            
+		if (target4 > 0):
+			motor4.forward(target4)
+		elif (target4 < 0): 
+			motor4.backward(-target4)
+		else:
+			motor4.stop()
